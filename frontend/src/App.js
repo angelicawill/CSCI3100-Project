b9d65cf4628a4351d0a5434c8a5c61ebb8d829e7
angelicawill <angelicawillianto@gmail.com>
2021-03-16 18:32:30 +0700

    commit landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/pages/LandingPage/LandingPage';
+import Form from './components/pages/Forms/Form';
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-      <Route path='/' exact component={LandingPage} />        
+      <Route path='/' exact component={LandingPage} />    
+      <Route path='/sign-up' exact component={Form} />       
       </Switch>      
     </Router>
   );
