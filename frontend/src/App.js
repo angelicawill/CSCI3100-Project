9efff9709ae2bf3ca42d82f5451579dfe275f25a
angelicawill <angelicawillianto@gmail.com>
2021-03-16 22:12:40 +0700

    Revert "commit landing page"
    This reverts commit b9d65cf4628a4351d0a5434c8a5c61ebb8d829e7.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LandingPage from './components/pages/LandingPage/LandingPage';
-import Form from './components/pages/Forms/Form';
 
 
 function App() {
@@ -11,8 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Switch>
-      <Route path='/' exact component={LandingPage} />    
-      <Route path='/sign-up' exact component={Form} />       
+      <Route path='/' exact component={LandingPage} />        
       </Switch>      
     </Router>
   );
--- frontend/src/components/pages/LandingPage/LandingPage.js
+++ frontend/src/components/pages/LandingPage/LandingPage.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import HeroSection from './../../HeroSection';
-import image from './image-landing-page.png';
+import './LandingPage.css';
+import { Button } from './../../Button.css';
+import { Link } from 'react-router-dom';
+import {ReactComponent as Image} from './image-landing-page.svg'
 
-
-const homeObjOne = {
-  lightBg: true,
-  lightText: false,
-  lightTextDesc: false,
-  headline: `Find the perfect tutor for you`,
-  description:
-    'We will help you connect and arrange meetings with the best tutors in Hong Kong to learn a variety of different subjects and skills. ',
-  buttonLabel: 'Get Started',
-  imgStart: '',
-  img: './image-landing-page.png',
-  alt: 'Teacher'
-};
 
 
 function LandingPage() {
   return (
-    <>      
-        <HeroSection {...homeObjOne} />  
+    <>
+      <div
+        className='lightBg'
+      >
+        <div className='container'>
+          <div
+            className='row home__hero-row'
+            style={{
+              display: 'flex',
+              flexDirection: 'row'
+            }}
+          >
+            <div className='col'>
+              <div className='home__hero-text-wrapper'>
+                
+                <h1 className={'heading dark'}>
+                  'Find the perfect'
+                </h1>
+                <h1 className={'heading dark'}>
+                  'tutor for you'
+                </h1>
+                <p
+                  className={'home__hero-subtitle dark'}
+                >
+                  'We will help you connect and arrange meetings with the best tutors in Hong Kong to learn a variety of different subjects and skills. '
+                </p>
+                <Link to='/sign-up'>
+                  <Button buttonSize='btn--medium' buttonColor='green'>
+                  'Get Started'
+                  </Button>
+                </Link>
+              </div>
+            </div>
+            <div className='col'>
+              <div className='home__hero-img-wrapper'>
+                <Image alt= 'Teacher' className='home__hero-img' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
