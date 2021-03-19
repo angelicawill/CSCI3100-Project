f91cc980805e2d2a91a7fc172572604ec44a9fa3
matszyan0727 <62585305+matszyan0727@users.noreply.github.com>
2021-03-19 19:15:47 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/Navbar';
 import LandingPage from './components/pages/LandingPage/LandingPage';
 import SignUpForm from './components/pages/SignUpForm/Form';
 import LoginForm from './components/pages/LoginForm/Form';
+import FindTutorPage from "./components/pages/FindTutorPage/FindTutorPage";
+import Header from "./components/Header";
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
       <Route path='/' exact component={LandingPage} />    
       <Route path='/sign-up' exact component={SignUpForm} />    
       <Route path='/login' exact component={LoginForm} />     
+      <Route path='/find-tutor' exact component={FindTutorPage} />
       </Switch>      
     </Router>
   );
