b4a4be5539f0bb69260cd3ae6184051adae7234f
shampoo Ma <a0017452406@gmail.com>
2021-03-22 22:46:39 +0800

    updated chatroom and added test files

---
+++ backend/chatroom/getRoom.js
@@ -0,0 +1,94 @@
+let { users, chats } = require('../test/testdata');
+const util = require('util')
+
+module.exports = ({
+    socket,
+    currentUser,
+    useridSocket,
+    io
+}) => {
+    let roomname = 'get rooms';
+    socket.on(roomname, ({
+        roomIndexes = null,
+        roomid = null,
+    }) => {
+        let returnObject = {
+            rooms: [],
+            success: false,
+            roomIndexesValid: false,
+            roomidValid: false,
+            canFindRooms: false,
+            userIsInRoom: false,
+            serverError: true,
+        }
+
+        let valid = true;
+        try {
+            // Check roomIndexes and roomid valid
+            console.log(roomIndexes)
+            if (Array.isArray(roomIndexes) && roomIndexes.length == 2 && roomIndexes.every((index) => typeof index == "number" && Number.isInteger(index)) && roomIndexes[0] >= 0 && roomIndexes[1] >= roomIndexes[0]) {
+                returnObject.roomIndexesValid = true;
+            }
+
+            if (typeof roomid == "number" && Number.isInteger(roomid)) {
+                returnObject.roomidValid = true;
+            }
+
+            if (!returnObject.roomIndexesValid && !returnObject.roomidValid) {
+                valid = false;
+            }
+
+            if (valid) {
+                let dataExist = true;
+
+                // Check have rooms
+                let canFindrooms = true;
+                let roomIdList;
+                if (returnObject.roomIndexesValid) {
+                    roomIdList = currentUser.roomids.slice(roomIndexes[0], roomIndexes[1] + 1);
+                } else {
+                    roomIdList = [roomid];
+                }
+                if (roomIdList.length == 0) {
+                    canFindrooms = false;
+                }
+                console.log(roomIdList);
+
+                let rooms = roomIdList.map((id) => {
+                    let room = chats.find(room => room.id == id);
+                    if (!room) {
+                        canFindrooms = false;
+                    }
+                    return room;
+                })
+
+                if (canFindrooms) {
+                    returnObject.canFindRooms = true;
+                } else {
+                    dataExist = false;
+                }
+
+                // Check user is in room
+                if (rooms.length && rooms.every(room => room.users.find((user => user.userid == currentUser.userid)))) {
+                    returnObject.userIsInRoom = true;
+                } else {
+                    dataExist = false;
+                }
+
+                if (dataExist) {
+                    returnObject.rooms = rooms;
+                    success = true;
+                }
+
+            }
+
+            returnObject.serverError = false;
+        } catch (error) {
+            console.log(error)
+        } finally {
+            socket.emit(roomname, returnObject);
+        }
+        // console.log(util.inspect(users, { showHidden: false, depth: null }))
+        // console.log(util.inspect(chats, { showHidden: false, depth: null }))
+    })
+}
---
+++ backend/test/chat.js
@@ -0,0 +1,103 @@
+function Chat() {
+    const [connect, setConnect] = useState(null);
+    const [socket, setSocket] = useState(null);
+    const [addtoroomText, setaddtoroomText] = useState(null);
+    const [createroomText, setcreateroomText] = useState(null);
+    const [sendmessageText, setsendmessageText] = useState(null);
+    const [getroomsText, setgetroomsText] = useState(null);
+    const [readmessageText, setreadmessageText] = useState(null);
+    const [error, setError] = useState(null);
+    function connectButton() {
+        let so = io('/chatroom');
+
+        so.on('connect', (data) => {
+            if (so.connected) {
+                setConnect("true");
+                setError(null);
+            }
+        })
+
+        so.on('add to room', (data) => {
+            console.log("received data from add to room in chat room")
+            setaddtoroomText(JSON.stringify(data))
+        })
+        so.on('create room', (data) => {
+            console.log("received data from creat room in chat room")
+            setcreateroomText(JSON.stringify(data))
+        })
+        so.on('send message', (data) => {
+            console.log("received data from send message in chat room")
+            setsendmessageText(JSON.stringify(data))
+        })
+        so.on('get rooms', (data) => {
+            console.log("received data from get rooms in chat room")
+            setgetroomsText(JSON.stringify(data))
+        })
+        so.on('read message', (data) => {
+            console.log("received data from read message in chat room")
+            setreadmessageText(JSON.stringify(data))
+        })
+        so.on("connect_error", (err) => {
+            setError(JSON.stringify(err));
+            setConnect(null);
+        });
+
+        setSocket(so);
+    }
+
+    function addRoomButton() {
+        let addtoroomInput = document.querySelector("#addtoroomInput");
+        let data = JSON.parse(addtoroomInput.value ? addtoroomInput.value : "{}");
+        socket.emit('add to room', data);
+    }
+    function createRoomButton() {
+        let createroomInput = document.querySelector("#createroomInput");
+        let data = JSON.parse(createroomInput.value ? createroomInput.value : "{}");
+        socket.emit('create room', data);
+    }
+    function sendMessageButton() {
+        let sendmessageInput = document.querySelector("#sendmessageInput");
+        let data = JSON.parse(sendmessageInput.value ? sendmessageInput.value : "{}");
+        socket.emit('send message', data);
+    }
+    function getRoomButton() {
+        let getroomsInput = document.querySelector("#getroomsInput");
+        let data = JSON.parse(getroomsInput.value ? getroomsInput.value : "{}");
+        socket.emit('get rooms', data);
+    }
+    function readMessageButton() {
+        let readmessageInput = document.querySelector("#readmessageInput");
+        let data = JSON.parse(readmessageInput.value ? readmessageInput.value : "{}");
+        socket.emit('read message', data);
+    }
+
+
+    return (<>
+        <h1>Connect</h1>
+        <p>{connect}</p>
+        <h1>Error</h1>
+        <p>{error}</p>
+        <button onClick={connectButton}>Connect button</button>
+        <br />
+        <input id="addtoroomInput" />
+        <p>{addtoroomText}</p>
+        <button onClick={addRoomButton} > addtoroombutton</button>
+        <br />
+        <input id="createroomInput" />
+        <p>{createroomText}</p>
+        <button onClick={createRoomButton} > createroombutton</button>
+        <br />
+        <input id="sendmessageInput" />
+        <p>{sendmessageText}</p>
+        <button onClick={sendMessageButton} > sendmessagebutton</button>
+        <br />
+        <input id="getroomsInput" />
+        <p>{getroomsText}</p>
+        <button onClick={getRoomButton} > getroomsbutton</button>
+        <br />
+        <input id="readmessageInput" />
+        <p>{readmessageText}</p>
+        <button onClick={readMessageButton} > readmessagebutton</button>
+        <br />
+    </>)
+}
