8e5caeae7787bd1eaca4b3bde9c6fe1fb9dd4e15
worstit9 <56362276+worstit9@users.noreply.github.com>
2021-04-30 23:08:29 +0800

    add comment and remove commented out unnecessary code

--- backend/database/userGetter.js
+++ backend/database/userGetter.js
@@ -1,9 +1,20 @@
+/**
+ * The collection of functions is originally belongs in tutor.js and student.js respectively
+ * To avoid circular dependency, they are moved to here
+ * 
+ * getTutorData() return the document with a given tutor ID
+ * getStudentData() return the document with a given student ID
+ */
 const User = require("./model/user.model")
 const Case = require("./model/case.model")
 const Student = require("./model/student.model")
 const Tutor = require("./model/tutor.model")
 
-
+/**
+ * return the document that list a tutor's locations, subject etc...
+ * @param {Number} tutor 
+ * @returns {Object} object represent a tutor
+ */
 const getTutorData = async (tutorid) => {
   const x = await Tutor.findOne({tutorid:tutorid}).exec()
   if(x == null){
@@ -11,7 +22,11 @@ const getTutorData = async (tutorid) => {
   }
   return x
 }
-
+/**
+ * return the document that list a tutor's locations, subject etc...
+ * @param {Number} studentid 
+ * @returns {Object} object represent a student
+ */
 const getStudentData = async (studentid) => {
   const x = await Student.findOne({studentid:studentid}).exec()
   if(x == null){
